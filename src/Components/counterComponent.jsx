031ee1d387edfee3a04c59543a2a372e6a947a9a
AndreAlcantara <andrealcantara0720gmail.com>
2018-11-25 15:08:24 -0500

    first commit

---
+++ src/Components/counterComponent.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+
+class Counter extends Component{
+    state = {
+        count: 2,
+        tags: ['tag1', 'tag2', 'tag3']
+        
+    };
+
+   renderTags(){
+       if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+       else return  <ul>{this.state.tags.map(tag => <li key={tag}>tag</li>)}</ul>;
+   }
+    render(){
+       
+        return (
+        <React.Fragment>
+            
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className= 'btn btn-secondary btn-sm'>Increment</button>
+          {this.renderTags()}
+        </React.Fragment>);
+    }
+    getBadgeClasses() {
+        let classes = "badge m-2 ";
+        classes += this.state.count === 0 ? 'badge-warning' : 'badge-primary';
+        return classes;
+    }
+
+    formatCount(){
+        const {count} = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+
+       
+    
+}
+
+export default Counter;
